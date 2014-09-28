bb8742176df08daef6a7c0f5a218ebe946ba38e3
Derya KAYA <derya.kaya@bil.omu.edu.tr>
2014-09-28 23:05:54 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+   
+   <head>
+       <meta charset="UTF-8">
+   <title>Giriş Yapınız</title>
+            <body  background="http://i.hizliresim.com/nGmqlR.png"> 
+            </body>
+</body>  
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+<font face="Verdana,sans-serif" size=+2" color="#083873"> <div align="right"> 
+   <b>Giriş</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;
+</div></font>
+
+<br><br>
+
+<font face="Verdana,sans-serif" size=+1" color="#333333"> <div align="right">
+    <form action="login.php" method="POST">
+            
+            Kullanıcı Adı : <input type="text" name="username"/><br/>
+            Şifre : <input type="password" name="password"/><br/>
+            <input type="submit" value="Giriş">
+            
+            
+            
+            
+         
+            
+            
+            </form>
+</head>
+<body>
+   <a id="HyperLink1" href="/UniFrame/lostpassword/default.aspx?lang=tr-TR" style="text-decoration: underline; margin-top: 12px; margin-bottom: 12px; display: block;">İlk şifrenizi oluşturmak i&#231;in tıklayınız.</a>
+
+   <?php
+    echo 'Tarih: ' . date('d.m.Y H:i:s');
+   ?>
+</body>
+</html>

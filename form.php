212e56818271541873144299aa8bed7f6c1291bb
Bengü Özcan <bengu.ozcan@bil.omu.edu.tr>
2015-03-30 01:43:19 +0300

    Form eklendi.

---
+++ form.php
@@ -0,0 +1,60 @@
+<html>
+<center>
+<head>
+	<meta http-equiv = "Content-Type" content = "text/html; charset=utf-8" />
+</head>
+<body>
+<form id = "form1" name = "form1" method = "post" action = "">
+	<table bgcolor="#66FFFF" width="300" border = "0" cellspacing = "0" cellpadding = "0">
+    <tr>
+		<td height="85" colspan="20" align="center">UYE KAYIT FORMU </td>
+	</tr>
+	<tr>
+		<td>Ad:</td>
+		<td><label>
+			<input name = "ad" type = "text" id = "ad" />
+		</label></td>
+    </tr>
+	<tr>
+		<td>Soyadınız</td>
+		<td><label>
+			<input name = "soyad" type = "text" id = "soyad" />
+		</label></td>
+    </tr>
+	<tr>
+		<td>Kulanıcı Adı: </td>
+		<td><label>
+			<input name="Kullanici Adi" type="text" id="kulaniciad" />
+		</label></td>
+    </tr>
+    <tr>
+      <td>E-posta</td>
+      <td><label>
+        <input name="eposta" type="text" id="eposta" />
+      </label></td>
+    </tr>
+
+    <tr>
+		<td>Şifre:</td>
+		<td><label>
+			<input name="sifrem" type="password" id="sifrem" size="20" maxlength="10" />
+		</label></td>
+    </tr>
+	<tr>Şifre Tekrar: </td>
+      <td><label>
+			<input name = "sifre" type = "password" id = "sifre" size = "20" maxlength = "10" />
+      </label></td>
+    </tr>
+	<tr>
+		<td><label> <br />
+			<input name = "KAYDET" type = "submit" id = "KAYDET" value = "KAYDET" />
+		</label></td>
+		<td><label> <br />
+			<input name = "SIL" type = "reset" id = "SIL" value = "SIL" />
+      </label></td>
+   </tr>
+</table>
+</form>
+</body>
+</center>
+</html>
